c466e54de1cbb0ca8f5a2d31657ad7d9fbeb947f
HiroOnoda <danilaprime2000@yandex.ru>
2022-04-03 18:40:46 +0300

    v1

---
+++ Calc/src/Calc.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+	double a,b,r;
+	//a,b-Хранят введенные числа
+	char op;//хранит знак операции
+	printf("list of available operations :\n+  addition \n - subtraction \n * multiplication \n / division \n ! factorial (of n1) \n e n1 to the power of n2 \n");
+	//список доступных пользователю операций,символ в начале строки вводим при запросе выбора операции
+	do
+	{
+		printf("enter first number \n");
+		//Считываем число 2 внутри Switch по причине того, что в некоторых операциях, например при вычислении факториала второе число не нужно
+	    scanf("%lf", &a);
+		printf("select the operator \n");
+		scanf(" %c", &op);
+		switch(op)
+			{
+			    case '+':
+					    printf("enter second number \n");
+					    scanf("%lf", &b);
+					    printf("%lf \n",a+b);
+					    break;
+			    case '-':
+			    	    printf("enter second number \n");
+			    		scanf("%lf", &b);
+			    		printf("%lf \n",a-b);
+			    		break;
+			    case '*':
+			    	    printf("enter second number \n");
+			    	    scanf("%lf", &b);
+			    	    printf("%lf \n",a*b);
+			    	    break;
+			    case '/':
+			    	    printf("enter second number \n");
+			    		scanf("%lf", &b);
+			    		printf("%lf \n",a/b);
+			    		break;
+			    case 'e':
+			    		printf("enter second number \n");
+			    		scanf("%lf", &b);
+			    		r = a;
+			    		//проверка на целость a и b
+			    		do
+			    		{
+			    			r=--r;
+			    		}while(!(r < 1));
+			    		if(r == 0)//если a-целое, то:
+			    		{
+			    			//printf("%lf \n",a);
+			    			r = b;
+			    			do
+			    			{
+			    				r=--r;
+			    			}while(!(r < 1));
+			    			if(r == 0)//если b-целое, то:
+			    			{
+			    				r = a;
+			    				for(float i = 1; i<b;i++)
+			    				{
+			    					r=r*a;
+			    				}
+			    				printf("%lf \n",r);
+			    			}
+			    		}
+			    		break;
+			    case '!'://здесь незадействованную b использую как счетчик для результата
+			    	    b = 1;
+			    	    for(int i = 1;i <= a;i++)
+			    		{
+			    		    b = b*i;;
+			    		}
+			    		printf("%lf \n",b);
+			    		break;
+			    default:
+			    		printf("no such operation is available \n");
+			}
+		    printf("do you wish to do another operation?(Y) \n");//Завершит цикл работы калькулятора
+			scanf(" %c", &op);                                     //при вводе любого символа кроме 'Y'
+	}while(op == 'Y');                                             //Вложил в переменную водящую в знак
+	                                                               //Т.к. на этом этапе работы она уже не используется
+
+	return EXIT_SUCCESS;
+}
